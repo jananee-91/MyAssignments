474785491c7936623a1f66632ad12b8be15295f2
jananee-91 <jananee.r1@gmail.com>
2025-02-04 22:41:32 -0500

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		String developer ="Google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(beta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+	}
+
+}
